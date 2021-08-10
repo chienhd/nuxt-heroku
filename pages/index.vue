6bf7a5529b76783cbda4d6571a9bdd731afc9492
chienhd <chienhd@sudobo.com>
2021-08-10 17:46:07 +0700

    pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <h1>Hello Hoang Dinh Chien</h1>
-    <Tutorial/>
+  <p v-if="$fetchState.pending">Fetching mountains...</p>
+  <p v-else-if="$fetchState.error">An error occurred :(</p>
+  <div v-else>
+    <h1>Nuxt Mountains</h1>
+    <ul>
+      <li v-for="mountain of mountains">{{ mountain.title }}</li>
+    </ul>
+    <button @click="$fetch">Refresh</button>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        mountains: []
+      }
+    },
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/mountains'
+      ).then(res => res.json())
+    }
+  };
 </script>
